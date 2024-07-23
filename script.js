624c1ac9a6fc94ac4ee1da571aad21a190e0073a
Dionne Shaw <juli_berk@yahoo.com>
2024-07-23 16:41:25 -0400

    Regex Checker App

---
+++ script.js
@@ -0,0 +1,13 @@
+function regexChecker() {
+  const firstName = document.getElementById('firstName').value;
+  const lastName = document.getElementById('lastName').value;
+  const testFirstLastLenth = /^[A-Z][a-z]+$/;
+
+
+  
+  if (firstName.match (testFirstLastLenth) && lastName.match (testFirstLastLenth) ) {
+    alert("Yay! Your inputs were all correct!");
+  } else {
+    alert("Oh no! That's an invalid format!");
+  }
+}
