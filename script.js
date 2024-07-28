efe5c878823de13c89ba437d7b093b9e042b1ff5
Dionne Shaw <juli_berk@yahoo.com>
2024-07-28 12:45:47 -0400

    Deleted unnecessary spaces

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ function regexChecker() {
   const firstName = document.getElementById('firstName').value;
   const lastName = document.getElementById('lastName').value;
   const testFirstLastLenth = /^[A-Z][a-z]+$/;
-
-
   
   if (firstName.match (testFirstLastLenth) && lastName.match (testFirstLastLenth) ) {
     alert("Yay! Your inputs were all correct!");
